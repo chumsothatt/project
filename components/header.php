6277081226079ed81ba4d963a5812783df1bc5e3
Sothatt <chhumsothatt353@gmail.com>
2025-06-13 17:41:13 +0700

    Add files via upload

---
+++ components/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+    <link rel="stylesheet" href="assets/bootstrap-5.0.2-dist/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <header>
---
+++ components/navbar.php
@@ -0,0 +1,39 @@
+<div class="nav-top w-100 py-2" style="background-color: rgb(249, 45, 45);"></div>
+<nav class="navbar navbar-expand-lg navbar-light  py-3" style="background-color: rgb(235, 231, 231);">
+
+    <div class="container">
+        <a class="navbar-brand " href="#">
+            <img src="images/logo.png" alt="" class="w-100">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link fw-bold hvr-shutter-out-horizontal" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link fw-bold  hvr-shutter-out-horizontal" href="#">Product</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link fw-bold hvr-shutter-out-horizontal" href="#">
+                        New product
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link fw-bold hvr-shutter-out-horizontal" href="#">about</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link fw-bold hvr-shutter-out-horizontal" href="#">Contact us</a>
+                </li>
+            </ul>
+            <div class="login">
+                <a href="" class="btn btn-danger">Login</a>
+                <a href="" class="btn btn-outline-success">Create</a>
+            </div>
+        </div>
+    </div>
+</nav>
+</header>
